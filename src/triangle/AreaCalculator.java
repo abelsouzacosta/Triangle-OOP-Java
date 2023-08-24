a04510362708a1daf072b32a77cbb32e078c6402
Junior <abelsouzacosta@gmail.com>
2023-08-24 05:35:22 -0300

    [refactor]: separation of concerns

---
+++ src/triangle/AreaCalculator.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package triangle;
+
+/**
+ *
+ * @author junior
+ */
+public class AreaCalculator {
+    private double semi_perimeter;
+    private double area;
+    
+    public AreaCalculator() {}
+    
+    /**
+     * Calculate the semi perimeter of the triangle
+     * @param a - first side of triangle
+     * @param b - second side of triangle
+     * @param c - third side of triangle
+     */
+    protected void calculateSemiPerimeter(double a, double b, double c) {
+        this.semi_perimeter = (a + b + c) / 2;
+    }
+    
+    protected void calculateArea(double a, double b, double c) {
+        this.calculateSemiPerimeter(a, b, c);
+        this.area = Math.sqrt(
+                this.getSemiPerimeter() *
+                (this.getSemiPerimeter() - a) *
+                (this.getSemiPerimeter() - b) *
+                (this.getSemiPerimeter() - c)
+        );
+    }
+    
+    protected double getSemiPerimeter() {
+        return this.semi_perimeter;
+    }
+    
+    protected double getArea() {
+        return this.area;
+    }
+    
+}
---
+++ test/triangle/AreaCalculatorTest.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package triangle;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author junior
+ */
+public class AreaCalculatorTest {
+    
+    private AreaCalculator calculator;
+    
+    public AreaCalculatorTest() {}
+    
+    @Before
+    public void setUp() {
+        this.calculator = new AreaCalculator();
+    }
+
+    @Test
+    public void testCalculateSemiPerimeter() {
+        this.calculator.calculateSemiPerimeter(8, 8, 8);
+        assertEquals(12, this.calculator.getSemiPerimeter(), 0.0f);
+    }
+    
+    @Test
+    public void testCalculateArea() {
+        this.calculator.calculateArea(4, 13, 15);
+        assertEquals(24, this.calculator.getArea(), 0.0f);
+    }
+    
+}
