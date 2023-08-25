84e4a79ceafda603f3c5707cf9e0bf6909e3fd0f
junior <junior@pop-os.localdomain>
2023-08-24 22:25:03 -0300

    [refactor]: turning triangle properties final

--- src/triangle/Triangle.java
+++ src/triangle/Triangle.java
@@ -9,9 +9,9 @@ package triangle;
  * @author junior
  */
 public class Triangle {
-    private double a;
-    private double b;
-    private double c;
+    private final double a;
+    private final double b;
+    private final double c;
     
     public Triangle(double a, double b, double c) {
         SideValidator.areSidesPositiveValues(a, b, c);
@@ -22,24 +22,12 @@ public class Triangle {
         this.c = c;
     }
     
-    protected void setA(double a) {
-        this.a = a;
-    }
-    
     protected double getA() {
         return this.a;
     }
     
-    protected void setB(double b) {
-        this.b = b;
-    }
-    
     protected double getB() {
         return this.b;
-    }
-    
-    protected void setC(double c) {
-        this.c = c;
     }
     
     protected double getC() {
